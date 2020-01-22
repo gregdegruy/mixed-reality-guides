a4706c0e669e7da4760d1d28bfba776c68f365d3
Greg <gregory.degruy@gmail.com>
2020-01-22 11:28:09 -0800

    + create plugin over the command line

--- GuidesDemo/GuidesDemo/SamplePlugin.cs
+++ GuidesDemo/GuidesDemo/SamplePlugin.cs
@@ -31,11 +31,8 @@ namespace Dynamics.SpurGo.Gamification
 
             if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
             {
-                // Obtain the target entity from the input parameters.  
                 Entity entity = (Entity)context.InputParameters["Target"];
-
-                // Obtain the organization service reference which you will need for  
-                // web service calls.  
+ 
                 IOrganizationServiceFactory serviceFactory =
                     (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
                 IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
---
+++ create_solution.bat
@@ -0,0 +1,8 @@
+pac auth create --url https://msott.crm.dynamics.com
+
+# cd to RevTwoGuides dir
+# local solution xml file
+pac solution init --publisher-name RevTwo --publisher-prefix revtwo
+
+# cd to guides plugin dir
+pac plugin init
