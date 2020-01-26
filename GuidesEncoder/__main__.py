2b3d7ae9b30827de21071e0d5ffad4289a17248e
Greg <gregory.degruy@gmail.com>
2020-01-25 19:43:20 -0800

    + in out works now, resolve data type issue

--- GuidesEncoder/__main__.py
+++ GuidesEncoder/__main__.py
@@ -1,7 +1,7 @@
 import base64
 import json
 
-with open('C:\\Users\\grego\\Documents\\GitHub\\greg\\guides-entity-temp\\GuidesEncoder\\works.json', encoding="utf8") as jsonfile:
+with open('C:\\Users\\grego\\Documents\\GitHub\\greg\\guides-entity-temp\\GuidesEncoder\\in.json', encoding="utf8") as jsonfile:
     data = json.load(jsonfile)
     datastr = json.dumps(data)
     encoded = base64.b64encode(datastr.encode('utf-8'))
