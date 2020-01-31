27e85d622452f502cdb49ea75c4b9bc1760b5067
Greg <gregory.degruy@gmail.com>
2020-01-30 16:40:32 -0800

    ! bugs + port azure msal from web app to backend app package

--- GuidesEncoder/webapp/app.py
+++ GuidesEncoder/webapp/app.py
@@ -6,12 +6,20 @@ import uuid
 
 from guidesencoder.config import DevConfig
 
+try:
+    from guidesencoder.app.msmrw.azureauth import AzureAuth
+except ImportError:
+    import sys
+    sys.path.append('.')
+    from guidesencoder.app.msmrw.azureauth import AzureAuth
+
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 Session(app)
+azureAuth = AzureAuth()
 
 @app.route("/")
-def index():
+def index():    
     if not session.get("user"):
         return redirect(url_for("login"))
     return render_template('index.html', user=session["user"], version=msal.__version__)
@@ -30,13 +38,19 @@ def authorized():
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
-        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
+        # result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
+        #     request.args['code'],
+        #     scopes=DevConfig.SCOPE,
+        #     redirect_uri=url_for("authorized", _external=True))
+        # if "error" in result:
+        result = azureAuth.confidentialClient.acquire_token_by_authorization_code(
             request.args['code'],
             scopes=DevConfig.SCOPE,
             redirect_uri=url_for("authorized", _external=True))
         if "error" in result:
             return render_template("auth_error.html", result=result)
-        session["user"] = result.get("id_token_claims")
+        # session["user"] = result.get("id_token_claims")
+        session["user"] = azureAuth.confidentialClient.get("id_token_claims")
         _save_cache(cache)
     return redirect(url_for("index"))
 
@@ -76,34 +90,49 @@ def postGuides():
     return render_template('display.html', result=str("Post complete" + graph_data.text))
 
 def _load_cache():
-    cache = msal.SerializableTokenCache()
     if session.get("token_cache"):
-        cache.deserialize(session["token_cache"])
-    return cache
-
+        azureAuth.cache.deserialize(session["token_cache"])
+    return azureAuth.cache
+    # cache = msal.SerializableTokenCache()
+    # if session.get("token_cache"):
+    #     azureAuth.cache.deserialize(session["token_cache"])
+    #     cache.deserialize(session["token_cache"])
+    # return cache    
+    
 def _save_cache(cache):
-    if cache.has_state_changed:
-        session["token_cache"] = cache.serialize()
+    if azureAuth.cache.has_state_changed:
+        azureAuth.cache = cache
+        session["token_cache"] = azureAuth.cache.serialize()
+    # if cache.has_state_changed:
+        # session["token_cache"] = cache.serialize()
 
 def _build_msal_app(cache=None, authority=None):
-    return msal.ConfidentialClientApplication(
-        DevConfig.CLIENT_ID, authority=authority or DevConfig.AUTHORITY_MULTI_TENANT,
-        client_credential=DevConfig.CLIENT_SECRET, token_cache=cache)
+    azureAuth.build_msal_app()
+    # return msal.ConfidentialClientApplication(
+    #     DevConfig.CLIENT_ID, 
+    #     authority=authority or DevConfig.AUTHORITY_MULTI_TENANT,
+    #     client_credential=DevConfig.CLIENT_SECRET, 
+    #     token_cache=cache)
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    return _build_msal_app(authority=authority).get_authorization_request_url(
-        scopes or [],
-        state=state or str(uuid.uuid4()),
-        redirect_uri=url_for("authorized", _external=True))
+    return azureAuth.get_auth_url(state, url_for("authorized", _external=True))
+    # return _build_msal_app(authority=authority).get_authorization_request_url(
+    #     scopes or [],
+    #     state=state or str(uuid.uuid4()),
+    #     redirect_uri=url_for("authorized", _external=True))
 
 def _get_token_from_cache(scope=None):
-    cache = _load_cache() 
-    cca = _build_msal_app(cache=cache)
-    accounts = cca.get_accounts()
-    if accounts:  # So all account(s) belong to the current signed-in user
-        result = cca.acquire_token_silent(scope, account=accounts[0])
+    cache = _load_cache()
+    if azureAuth.confidentialClient.get_accounts():
+        result = azureAuth.confidentialClient.acquire_token_silent(scope, account=azureAuth.confidentialClient.get_accounts()[0])
         _save_cache(cache)
-        return result
+        return result 
+    # cca = _build_msal_app(cache=cache)
+    # accounts = cca.get_accounts()
+    # if accounts:  # So all account(s) belong to the current signed-in user
+    #     result = cca.acquire_token_silent(scope, account=accounts[0])
+    #     _save_cache(cache)
+    #     return result
 
 app.jinja_env.globals.update(_build_auth_url=_build_auth_url) 
 
---
+++ guidesencoder/app/msmrw/azureauth.py
@@ -0,0 +1,43 @@
+﻿import msal # works for api with application permissions
+
+try:
+    from guidesencoder.config import DevConfig
+except ImportError:
+    import sys
+    sys.path.append('.')
+    from guidesencoder.config import DevConfig
+
+class AzureAuth:
+    bearerToken = ""
+    cache = None
+    confidentialClient = None
+
+    def __init__(self):
+       self.cache = msal.SerializableTokenCache()
+       self.confidentialClient = msal.ConfidentialClientApplication(
+            DevConfig.CLIENT_ID, 
+            authority=DevConfig.AUTHORITY_MULTI_TENANT,
+            client_credential=DevConfig.CLIENT_SECRET, 
+            token_cache=self.cache)
+
+    def build_msal_app(self, cache=None):
+        self.confidentialClient = msal.ConfidentialClientApplication(
+            DevConfig.CLIENT_ID, 
+            authority=DevConfig.AUTHORITY_MULTI_TENANT,
+            client_credential=DevConfig.CLIENT_SECRET, 
+            token_cache=self.cache)
+    
+    def get_auth_url(self, state=None, redirect_uri=None):
+        return self.confidentialClient.get_authorization_request_url(
+            DevConfig.SCOPE or [],
+            state=state or str(uuid.uuid4()),
+            redirect_uri=redirect_uri)
+    
+    def get_token_from_cache(self, cache):
+        if self.confidentialClient.get_accounts():
+            result = self.confidentialClient.acquire_token_silent(scope, account=self.confidentialClient.get_accounts()[0])
+            _save_cache(cache)
+            return result
+    
+    def refreshToken():
+        pass
